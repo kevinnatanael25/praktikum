8c910bfa949db6431648774c88323c264e3f588e
Kevin Alfiah <natanael.kevin01@gmail.com>
2022-02-03 21:55:03 +0900

    ok

--- application/views/beranda_vw.php
+++ application/views/beranda_vw.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url()?>/assets/css/bootstrap.min.css">
+    <script src="<?php echo base_url()?>/assets/js/jquery-3.3.1.min.js"></script>
+    
     <title>Belajar CRUD</title>
 </head>
 <body>
